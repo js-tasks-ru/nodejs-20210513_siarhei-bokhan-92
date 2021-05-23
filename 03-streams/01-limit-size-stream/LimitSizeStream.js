3b1bee7e0cce243470a2d16b8b3323eee321cc2b
sbokhan <siarhei.bokhan.92@gmail.com>
2021-05-24 00:42:28 +0300

    feat: added tasks solution on the topic of streams

--- 03-streams/01-limit-size-stream/LimitSizeStream.js
+++ 03-streams/01-limit-size-stream/LimitSizeStream.js
@@ -2,12 +2,20 @@ const stream = require('stream');
 const LimitExceededError = require('./LimitExceededError');
 
 class LimitSizeStream extends stream.Transform {
+  #byteReadNumber = 0;
+  #limit = Infinity;
+
   constructor(options) {
     super(options);
+    this.#limit = options.limit;
   }
 
   _transform(chunk, encoding, callback) {
+    this.#byteReadNumber += chunk.length;
 
+    if (this.#byteReadNumber > this.#limit) return callback(new LimitExceededError());
+
+    callback(null, chunk);
   }
 }
 
--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -1,16 +1,34 @@
 const stream = require('stream');
 const os = require('os');
 
+const lineBreakRegExp = new RegExp(`${os.EOL}`);
+const endWithLineBreakRegExp = new RegExp(`${os.EOL}$`);
+
 class LineSplitStream extends stream.Transform {
-  constructor(options) {
-    super(options);
-  }
+	#buffer = "";
 
-  _transform(chunk, encoding, callback) {
-  }
+	constructor(options) {
+		super(options);
+	}
 
-  _flush(callback) {
-  }
+	_transform(chunk, encoding, callback) {
+		const chunkString = chunk.toString();
+    this.#buffer += chunkString;
+
+    if (!lineBreakRegExp.test(chunkString)) return callback(); 
+
+    let lines = this.#buffer.split(os.EOL);
+    this.#buffer = endWithLineBreakRegExp.test(chunkString) ? "" : lines.pop();
+    lines.forEach(line => this.push(line));
+
+    callback();
+	}
+
+	_flush(callback) {
+    if (this.#buffer !== "") this.push(this.#buffer);
+
+		callback();
+	}
 }
 
 module.exports = LineSplitStream;
