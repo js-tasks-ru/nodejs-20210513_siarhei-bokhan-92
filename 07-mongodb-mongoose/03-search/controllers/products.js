15bcb6a6fa6b40138595a1e4e44c6100587c3686
sbokhan <siarhei.bokhan.92@gmail.com>
2021-06-13 01:24:43 +0300

    feat: added solution of mongo tasks

--- 07-mongodb-mongoose/03-search/controllers/products.js
+++ 07-mongodb-mongoose/03-search/controllers/products.js
@@ -1,3 +1,18 @@
+const Product = require("../models/Product");
+
+const toClient = data => {
+	const {_id: id, __v, ...rest} = data;
+	return {...rest, id};
+};
+
 module.exports.productsByQuery = async function productsByQuery(ctx, next) {
-  ctx.body = {products: []};
+  const query = ctx.request.query.query;
+
+  if (!query) ctx.throw(501); 
+
+  const products = await Product.find( { $text: { $search: query } } ).lean();
+
+  ctx.body = {
+    products: products.map(toClient),
+  };
 };
